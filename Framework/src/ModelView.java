45b42e94894e4084fee884a9734b08f0baa76344
Stirio273 <119032157+Stirio273@users.noreply.github.com>
2023-04-18 05:40:37 +0300

    sprint6

--- Framework/src/ModelView.java
+++ Framework/src/ModelView.java
@@ -5,12 +5,16 @@
  */
 package etu1832.framework;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author itu
  */
 public class ModelView {
     String vue;
+    HashMap<String, Object> data;
 
     public String getVue() {
         return vue;
@@ -20,7 +24,25 @@ public class ModelView {
         this.vue = vue;
     }
 
+    public HashMap<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(HashMap<String, Object> data) {
+        this.data = data;
+    }
+
     public ModelView(String vue) {
         this.vue = vue;
+        this.data = new HashMap<>();
+    }
+
+    public ModelView(String vue, HashMap<String, Object> data) {
+        this.vue = vue;
+        this.data = data;
+    }
+
+    public void addItem(String key, Object value) {
+        this.data.put(key, value);
     }
 }
--- WebApplication/build/WEB-INF/vues/all.jsp
+++ WebApplication/build/WEB-INF/vues/all.jsp
@@ -1,8 +1,7 @@
-<%-- 
-    Document   : form
-    Created on : 28 mars 2023, 08:54:11
-    Author     : itu
---%>
+<%@ page import="modele.RouteNational,java.util.*" %>
+<%
+List<RouteNational> liste = (List<RouteNational>) request.getAttribute("listeRN");
+%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -13,5 +12,10 @@
     </head>
     <body>
         <h1>All</h1>
+        <ul>
+            <% for(int i = 0; i < liste.size(); i++) { %>
+                <li><% out.println(liste.get(i).getNom()); %></li>
+            <% } %>
+        </ul>
     </body>
 </html>
